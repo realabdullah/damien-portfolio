92a1aa305a2fb335e4253f21f8eb8e59bdd0a003
realabdullah <abdulodesanmi@gmail.com>
2025-01-09 14:53:18 +0100

    (chore): restructure layout to include testimonials, marquees, and footer in the root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,11 @@ import Header from "@/app/ui/Header";
 import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
 import "./globals.css";
+import Marquees from "./home/Marquees";
+import Footer from "./ui/Footer";
+import Image from "next/image";
+import TextLogo from "@/public/logo-text.svg";
+import Testimonials from "./home/Testimonials";
 
 const manrope = Manrope({
   variable: "--font-manrope",
@@ -33,7 +38,24 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${manrope.variable} bg-black`}>
         <Header />
+
         {children}
+
+        <div className="my-20 lg:my-[150px] xl:my-[200px] px-4 lg:px-[8.44vw]">
+          <Testimonials />
+        </div>
+
+        <div>
+          <div className="px-4 lg:px-[8.44vw] pb-5 lg:pb-[60px] xl:pb-[100px] ml-auto w-full">
+            <Image
+              src={TextLogo}
+              alt="logo"
+              className="ml-auto w-full max-w-[315px] lg:max-w-[926px] xl:max-w-[1058px]"
+            />
+          </div>
+          <Marquees />
+          <Footer />
+        </div>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,6 @@ import Services from "./home/Services";
 import Portfolio from "./home/Portfolio";
 import FAQs from "./home/FAQs";
 import Testimonials from "./home/Testimonials";
-import Image from "next/image";
-import TextLogo from "@/public/logo-text.svg";
-import Footer from "./ui/Footer";
 
 export default function Page() {
   return (
@@ -23,24 +20,13 @@ export default function Page() {
         </div>
       </div>
 
-      <div className="space-y-[200px] px-4 lg:px-[8.44vw]">
+      <div className="space-y-20 lg:space-y-[150px] xl:space-y-[200px] px-4 lg:px-[8.44vw]">
         <About />
         <Services />
         <Portfolio />
         <FAQs />
-        <Testimonials />
       </div>
-      <div>
-        <div className="px-4 lg:px-[8.44vw] pb-5 lg:pb-[60px] xl:pb-[100px] ml-auto w-full">
-          <Image
-            src={TextLogo}
-            alt="logo"
-            className="ml-auto w-full max-w-[315px] lg:max-w-[926px] xl:max-w-[1058px]"
-          />
-        </div>
-        <Marquees />
-        <Footer />
-      </div>
+
     </div>
   );
 }
